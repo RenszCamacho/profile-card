0d3f55346b8c4a4ead8307b419a78345869a1944
Renszo Camacho <varsovia84@gmail.com>
2021-01-02 16:12:29 +0100

    Card. Adding a Js counter animation.

---
+++ src/vendor/main.js
@@ -0,0 +1,20 @@
+const counters = document.querySelectorAll(".card__numbers");
+const speed = 400;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    const inc = target / speed;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + inc);
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
